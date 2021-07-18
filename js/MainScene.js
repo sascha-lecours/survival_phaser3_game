dc0ccd3d3080eb698900023d71545be86ac577a7
sascha-lecours <sascha.lecours@gmail.com>
2021-07-18 15:25:45 -0400

    refactors resource adding code

--- js/MainScene.js
+++ js/MainScene.js
@@ -1,4 +1,5 @@
 import Player from './Player.js';
+import Resource from './Resources.js';
 
 export default class MainScene extends Phaser.Scene {
     constructor(){
@@ -8,6 +9,7 @@ export default class MainScene extends Phaser.Scene {
     preload() {
         console.log("Preloading...");
         Player.preload(this);
+        Resource.preload(this);
         this.load.image('tiles', 'assets/images/RPG Nature Tileset.png');
         this.load.tilemapTiledJSON('map', 'assets/images/map.json');
     }
@@ -15,13 +17,18 @@ export default class MainScene extends Phaser.Scene {
     create(){
         // Tilemap setup
         const map = this.make.tilemap({ key: 'map' });
+        this.map = map;
         const tileset = map.addTilesetImage('RPG Nature Tileset', 'tiles', 32, 32);
         const layer1 = map.createStaticLayer('Tile Layer 1', tileset, 0, 0);
         const layer2 = map.createStaticLayer('Tile Layer 2', tileset, 0, 0);
         layer1.setCollisionByProperty({ collides: true });
         this.matter.world.convertTilemapLayer(layer1); // transmits collider zones to engine
 
-        // Player Setup
+        // Scenery setup
+        
+        this.map.getObjectLayer('Resources').objects.forEach(resource => new Resource({ scene: this, resource }));
+
+        // Player setup
         this.player = new Player({ scene: this, x: 70, y: 70, texture: 'thief', frame: 'thief_idle_1'});
         this.player.inputKeys = this.input.keyboard.addKeys({
             up: Phaser.Input.Keyboard.KeyCodes.W,
@@ -30,8 +37,6 @@ export default class MainScene extends Phaser.Scene {
             right: Phaser.Input.Keyboard.KeyCodes.D,
         });
 
-        //
-        
     }
 
     update(){
