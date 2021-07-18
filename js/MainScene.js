ab7182ddf38ca8cf576b523183f976cf09d6349f
sascha-lecours <sascha.lecours@gmail.com>
2021-07-18 14:34:09 -0400

    Added custom collision for water edges

--- js/MainScene.js
+++ js/MainScene.js
@@ -8,18 +8,30 @@ export default class MainScene extends Phaser.Scene {
     preload() {
         console.log("Preloading...");
         Player.preload(this);
+        this.load.image('tiles', 'assets/images/RPG Nature Tileset.png');
+        this.load.tilemapTiledJSON('map', 'assets/images/map.json');
     }
 
     create(){
-        console.log("Creating...");
-        this.player = new Player({ scene: this, x: 10, y: 10, texture: 'thief', frame: 'thief_idle_1'});
-        this.testPlayer = new Player({ scene: this, x: 40, y: 100, texture: 'thief', frame: 'thief_idle_1'});
+        // Tilemap setup
+        const map = this.make.tilemap({ key: 'map' });
+        const tileset = map.addTilesetImage('RPG Nature Tileset', 'tiles', 32, 32);
+        const layer1 = map.createStaticLayer('Tile Layer 1', tileset, 0, 0);
+        const layer2 = map.createStaticLayer('Tile Layer 2', tileset, 0, 0);
+        layer1.setCollisionByProperty({ collides: true });
+        this.matter.world.convertTilemapLayer(layer1); // transmits collider zones to engine
+
+        // Player Setup
+        this.player = new Player({ scene: this, x: 70, y: 70, texture: 'thief', frame: 'thief_idle_1'});
         this.player.inputKeys = this.input.keyboard.addKeys({
             up: Phaser.Input.Keyboard.KeyCodes.W,
             down: Phaser.Input.Keyboard.KeyCodes.S,
             left: Phaser.Input.Keyboard.KeyCodes.A,
             right: Phaser.Input.Keyboard.KeyCodes.D,
         });
+
+        //
+        
     }
 
     update(){
